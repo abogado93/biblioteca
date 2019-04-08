957db477a544f28796ee3178d2e20400deee45c3
abogado93 <aleboga93@hotmail.com>
2019-04-08 19:44:29 -0400

    agregando base de datos

--- proyecto.sql
+++ proyecto.sql
@@ -48,6 +48,148 @@ CREATE SEQUENCE public.usuarios_id_seq
     CACHE 1;
 
 
+
+
+
+CREATE TABLE "devoluciones" (
+
+"id" int4 NOT NULL DEFAULT nextval('devoluciones_id_seq'::regclass),
+
+"fecha_devolucion" date,
+
+"devolucion_devolucion" date,
+
+"persona_devolucion" numeric(50),
+
+"estado_devolucion" varchar(10),
+
+"id_libro_devolucion" numeric(50),
+
+CONSTRAINT "devoluciones_pkey" PRIMARY KEY ("id") 
+
+);
+
+
+
+CREATE TABLE "libros" (
+
+"id" int4 NOT NULL DEFAULT nextval('libros_id_seq'::regclass),
+
+"nombre_libro" varchar(200),
+
+"fecha_libro" date,
+
+"tipo_libro" numeric(50),
+
+"estado_libro" numeric(50),
+
+"precio_libro" numeric(50),
+
+"existencia_libro" numeric(50),
+
+"cantidad_libro" numeric(50),
+
+CONSTRAINT "libros_pkey" PRIMARY KEY ("id") 
+
+);
+
+
+
+CREATE TABLE "pagos" (
+
+"id" int4 NOT NULL DEFAULT nextval('pagos_id_seq'::regclass),
+
+"fecha_pago" date,
+
+"multa_pago" numeric(50),
+
+"monto_pago" numeric(50),
+
+"id_prestamo_pago" numeric(50),
+
+"estado_pago" varchar(100),
+
+CONSTRAINT "pagos_pkey" PRIMARY KEY ("id") 
+
+);
+
+
+
+CREATE TABLE "personas" (
+
+"id" int4 NOT NULL DEFAULT nextval('personas_id_seq'::regclass),
+
+"nombre_persona" varchar(100),
+
+"apellido_persona" varchar(100),
+
+"cedula_persona" numeric(50),
+
+"sexo_persona" varchar(10),
+
+"direccion_persona" varchar(200),
+
+CONSTRAINT "personas_pkey" PRIMARY KEY ("id") 
+
+);
+
+
+
+CREATE TABLE "prestamos" (
+
+"id" int4 NOT NULL DEFAULT nextval('prestamos_id_seq'::regclass),
+
+"fecha_pres" date,
+
+"estado_pres" varchar(50),
+
+"presona_pres" numeric(50),
+
+"libro_id_pres" numeric(50),
+
+"devolucion_pres" numeric(50),
+
+"dias_pres" numeric(50),
+
+"cantidad_pres" numeric(50),
+
+CONSTRAINT "prestamos_pkey" PRIMARY KEY ("id") 
+
+);
+
+
+
+CREATE TABLE "tipo_libros" (
+
+"id" int4 NOT NULL DEFAULT nextval('tipo_libros_id_seq'::regclass),
+
+"descripcion_tipo" varchar(100),
+
+CONSTRAINT "tipo_libros_pkey" PRIMARY KEY ("id") 
+
+);
+
+
+
+
+
+
+
+
+
+ALTER TABLE "libros" ADD CONSTRAINT "fk_libros_libros_1" FOREIGN KEY ("id") REFERENCES "devoluciones" ("id", "id_libro_devolucion");
+
+ALTER TABLE "tipo_libros" ADD CONSTRAINT "fk_tipo_libros_tipo_libros_1" FOREIGN KEY ("id") REFERENCES "libros" ("id", "tipo_libro");
+
+ALTER TABLE "libros" ADD FOREIGN KEY ("id") REFERENCES "prestamos" ("libro_id_pres");
+
+ALTER TABLE "personas" ADD CONSTRAINT "fk_personas_personas_1" FOREIGN KEY ("id") REFERENCES "prestamos" ("id", "presona_pres");
+
+ALTER TABLE "prestamos" ADD CONSTRAINT "fk_prestamos_prestamos_1" FOREIGN KEY ("id") REFERENCES "pagos" ("id", "id_prestamo_pago");
+
+
+
+
 ALTER TABLE ONLY public.clientes ALTER COLUMN id SET DEFAULT nextval('public.clients_id_seq'::regclass);
 
 
